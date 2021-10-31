0a4d789b3c245f30d30d43b9d4e32200b508de8a
Monish <monish07122001@gmail.com>
2021-10-31 22:23:22 +0530

    iCalc

--- src/Result.js
+++ src/Result.js
@@ -1,10 +1,19 @@
 import { useSelector } from "react-redux";
 import "./result.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { res } from "./Redux/Action";
 
 const Resultt = () => {
+  const dispatch = useDispatch();
   let Result = useSelector((state) => state.Re);
-  if (isNaN(Result)) Result = "MATH ERROR";
+
+  useEffect(() => {
+    if (isNaN(Result)) dispatch(res("MATH ERROR"));
+    // eslint-disable-next-line
+  }, [res]);
+
   return (
     <div>
       <div className="st">Result: {Result}</div>
