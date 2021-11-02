37040c68c59fcefe9d81711d124f128a9a8c61b0
Monish <monish07122001@gmail.com>
2021-11-02 09:10:05 +0530

    iCalc

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,27 @@
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import App from './App'
-import Resultt from './Result'
-import Number from './Number'
-import Operator from './operator'
+import App from "./App";
+import Resultt from "./Result";
+import Number from "./Number";
+import Operator from "./Operator";
 
 function Main() {
-  return(
+  return (
     <div className="Main">
       <Router>
         <Switch>
-          <Route path="/Result"><Resultt /></Route>
-          <Route path="/operator"><Operator /></Route>
-          <Route path="/Number"><Number /></Route>
-          <Route path="/"><App /></Route>
+          <Route path="/Result">
+            <Resultt />
+          </Route>
+          <Route path="/Operator">
+            <Operator />
+          </Route>
+          <Route path="/Number">
+            <Number />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
         </Switch>
       </Router>
     </div>
